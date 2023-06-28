9bef59b4f015eed5e7368d5f66f01b444785269d
yudiz-darshan-bhensadadiya <darshan.bhensadadiya@yudiz.com>
2023-06-28 10:39:19 +0530

    chnage style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './App.scss';
 import Dashboard from './Component/dashboard';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Summary from './Component/Summary';
@@ -16,7 +16,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
     <div className="App">
-      <h1 style={{background:'white', width:'fit-content',display: 'block' ,margin:'0px auto 15px'}}>Welcome to space X</h1>
+      <h1>Welcome to space X</h1>
       <Dashboard/>
       <Summary/>
     </div>
